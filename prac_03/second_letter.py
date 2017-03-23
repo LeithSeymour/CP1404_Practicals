42958a542b395afef7f3a1b6f252f8526ec92e01
leith01 <leith.seymour@my.jcu.edu.au>
2017-03-23 21:13:44 +1000

    second letter in name complete

--- prac_03/second_letter.py
+++ prac_03/second_letter.py
@@ -1,2 +1,22 @@
-user_name = input("Enter name: ")
-print(user_name[::2])
+def main():
+    user_name = get_name()
+    for_every = input("Enter integer to print every x number: ")
+    check = for_every.isdigit()
+    while not check:
+        for_every = input("Enter integer to print every x number: ")
+        check = for_every.isdigit()
+    else:
+        for_every = int(for_every)
+        print_name(user_name, for_every)
+
+
+def print_name(user_name, for_every):
+    print(user_name[::for_every])
+
+
+def get_name():
+    user_name = input("Enter name: ")
+    return user_name
+
+
+main()
