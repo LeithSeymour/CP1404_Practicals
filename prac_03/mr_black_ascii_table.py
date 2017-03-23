37dba3b86ea428961bf3bfd6f2e3631e168106ea
leith01 <leith.seymour@my.jcu.edu.au>
2017-03-23 20:34:16 +1000

    finished the function for mr black ascii

--- prac_03/mr_black_ascii_table.py
+++ prac_03/mr_black_ascii_table.py
@@ -1,24 +1,38 @@
-MIN_DIGIT = 33
-MAX_DIGIT = 127
-character = []
+def main():
+    # MAX_DIGIT = 33
+    # MIN_DIGIT = 127
+    # character = []
 
+    def get_number(lower, upper):
+        user_number = input("Enter number: ")
+        number_check = user_number.isdigit()
+        while not number_check:
+            user_number = input("Enter number: ")
+            number_check = user_number.isdigit()
+        else:
+            while not lower <= int(user_number) <= upper:
+                user_number = input("Enter number between {} & {}: ".format(lower, upper))
+                number_check = user_number.isdigit()
+                while not number_check:
+                    user_number = input("Enter number")
+            else:
+                return user_number
 
-def get_number():
-    character = input("Enter character: ")
-    character_int_check = character.isdigit()
+    number = get_number(10, 50)
+    check = number.isdigit()
 
+    if check:
+        number = int(number)
+        # while character < MIN_DIGIT or character > MAX_DIGIT:
+        #     character = int(input("Please enter new number between {} & {}: ".format(MIN_DIGIT, MAX_DIGIT)))
+        output = chr(number)
+        print("The character for {} is {}".format(number, output))
+    # else:
+    #     output = ord(character)
+    #     print("The character for {} is {}".format(character, output))
 
-while character.isdigit():
-    character = int(character)
-    while character < MIN_DIGIT or character > MAX_DIGIT:
-        character = int(input("Please enter new number between {} & {}: ".format(MIN_DIGIT, MAX_DIGIT)))
-    output = chr(character)
-    print("The character for {} is {}".format(character, output))
-else:
-    output = ord(character)
-    print("The character for {} is {}".format(character, output))
+    # for i in range(MIN_DIGIT, MAX_DIGIT):
+    #     print("{}{:>6}".format(i, chr(i)))
 
-for i in range(MIN_DIGIT, MAX_DIGIT):
-    print("{}{:>6}".format(i, chr(i)))
 
 main()
--- prac_03/practice_function.py
+++ prac_03/practice_function.py
@@ -1,3 +1,34 @@
-x = 'hello'
-print(x)
-"update test"
+# def get_number(lower, upper):
+#     number = input("Enter character: ")
+#     while number.isdigit():
+#         if lower < int(number) < upper:
+#             break
+#         else:
+#             number = input("Enter character: ")
+#     return(number)
+#             # character_int_check = character.isdigit()
+#
+#
+# number_received = get_number(10, 50)
+
+def get_number(lower, upper):
+    user_number = input("Enter number: ")
+    number_check = user_number.isdigit()
+    while not number_check:
+        user_number = input("Enter number: ")
+        number_check = user_number.isdigit()
+    else:
+        while not lower <= int(user_number) <= upper:
+            user_number = input("Enter number: ")
+            number_check = user_number.isdigit()
+            while not number_check:
+                user_number = input("Enter number")
+        else:
+            return user_number
+
+
+        # character_int_check = character.isdigit()
+
+
+number = get_number(10, 50)
+print(number)
