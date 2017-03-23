ac1f2c23d1337a43e9c86d443c682c61b53b6a2f
leith01 <leith.seymour@my.jcu.edu.au>
2017-03-23 21:53:24 +1000

    functioned fixed score

--- prac_03/fixed_score.py
+++ prac_03/fixed_score.py
@@ -2,6 +2,8 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+
+
 # if 100 >= score >= 90:
 #    print("Excellent")
 # elif 90 > score >= 50:
@@ -11,15 +13,19 @@ Broken program to determine score status
 # else:
 #    print("Invalid score")
 
+def return_score(score):
+    while score > 100 or score < 0:
+        print("Invalid")
+        score = float(input("Enter new score: "))
+    if score >= 90:
+        result = "Excellent"
+    elif score < 50:
+        result = "Bad"
+    else:
+        result = "Passable"
+    return result
+
+
 score = float(input("Enter score: "))
-
-while score > 100 or score < 0:
-    print("Invalid")
-    score = float(input("Enter new score: "))
-
-if score >= 90:
-    print("Excellent")
-elif score < 50:
-    print("Bad")
-else:
-    print("Passable")
+result = return_score(score)
+print(result)
